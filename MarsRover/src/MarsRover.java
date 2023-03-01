8a76e821bdedc8950b0a151ce1e9efc9b6dd22bb
Laura Voineag <lauravoineag87@gmail.com>
2023-03-01 16:33:38 +0000

    add implementation for moving 2 positions north

--- MarsRover/src/MarsRover.java
+++ MarsRover/src/MarsRover.java
@@ -1,5 +1,21 @@
+import java.text.MessageFormat;
+
 public class MarsRover {
+    private int _yPosition;
+    private int _xPosition;
+    private String _direction ="N";
+
     public String execute(String command) {
-        return "0:1:N";
+        if (command == "M")
+        {
+            _yPosition += 1;
+        }
+        else
+        {
+            _yPosition += 2;
+        }
+
+        return MessageFormat.format(
+                "{0}:{1}:{2}", _xPosition, _yPosition,_direction);
     }
 }
--- MarsRover/src/MarsRoverTest.java
+++ MarsRover/src/MarsRoverTest.java
@@ -9,7 +9,16 @@ class MarsRoverTest {
         //act
         var expected ="0:1:N";
         var actual=rover.execute("M");
-
+        //assert
+        assertEquals(expected,actual);
+    }
+    @Test
+    void when_I_pass_in_MM_I_receive_0_2_N_(){
+        //arrange
+        MarsRover rover = new MarsRover();
+        //act
+        var expected ="0:2:N";
+        var actual=rover.execute("MM");
         //assert
         assertEquals(expected,actual);
     }
